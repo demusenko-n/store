57b98550a36127445ad04621eba9ad07faaa2bc1
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-08-04 21:51:54 +0300

    Add Store.Memory project

---
+++ infrastructure/ClassLibrary1/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books =
+        {
+            new (1, "Art", "auth"),
+            new (2, "Brt", "buth"),
+            new (3, "Crt", "cuth"),
+            new (4, "Drt", "duth")
+        };
+
+        public IEnumerable<Book> GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
