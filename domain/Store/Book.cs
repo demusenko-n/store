e552e8c5d65d3ad5fbd61c045c3c1b185630de50
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-08-04 21:24:58 +0300

    .

---
+++ domain/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(int id, string title, string author)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+        }
+    }
+}
