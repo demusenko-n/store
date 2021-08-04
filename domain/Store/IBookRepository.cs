ef9215591368b52c121009f02877f01c9a14eb53
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-08-04 21:39:03 +0300

    Add interface

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace Store
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Store
 {
     public interface IBookRepository
     {
-        
+        IEnumerable<Book> GetAllByTitle(string titlePart);
     }
 }
