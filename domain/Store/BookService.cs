49ead64e0ce77e8b4bc51f3831905c3ac4601723
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-08-05 13:51:21 +0300

    Add book service class

---
+++ domain/Store/BookService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Store
+{
+    public class BookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IEnumerable<Book> GetAllByQuery(string query)
+        {
+            if (Book.IsIsbn(query))
+            {
+                return _bookRepository.GetAllByIsbn(query);
+            }
+
+            return _bookRepository.GetAllByTitleOrAuthor(query);
+        }
+
+    }
+}
