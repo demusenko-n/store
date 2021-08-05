c53bc89aa4a5365682865759c490ab2fd7eb2cd5
Демусенко Микита <mykyta.demusenko@nure.ua>
2021-08-05 13:51:03 +0300

    Add IsIsbn() method

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -5,13 +5,19 @@ namespace Store
     public class Book
     {
         public int Id { get; set; }
+        public string Isbn { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public Book(int id, string title, string author)
+        public Book(int id, string isbn, string author, string title)
         {
             Id = id;
+            Isbn = isbn;
             Title = title;
             Author = author;
+        }
+        internal static bool IsIsbn(string? query)
+        {
+            return query.Contains("ISBN ");
         }
     }
 }
